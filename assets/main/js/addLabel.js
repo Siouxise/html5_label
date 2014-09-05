cdebde14715cb0f56c04f7f16f8562a9b4543950
dannyzhou <zhoujian8585169@126.com>
2014-09-05 21:58:07 +0800

    html_create

---
+++ assets/main/js/addLabel.js
@@ -0,0 +1,21 @@
+/**
+ * Created by danny on 9/5/14.
+ */
+
+$(document).ready(function () {
+    $("#addLabel").click(function (e) {
+
+        var label_left;
+        // 添加一个元素  这里的 id 属性是临时的
+        $("#labelList").append('<div class="label_tag" id="labelTagAdded" style="top: 80px;"></div>');
+
+        // 这个地方填写 left 的值 ， 用 鼠标的位置 + bgShow 的 left 的值 然后减去一个常数。
+        // 这个地方的 bgShow 是 负值
+        label_left = e.pageX + (-$("#bgShow").offset().left) - 300;
+
+        // 更改刚添加的 label 的 left
+        $("#labelTagAdded").css("left",label_left+"px")
+            // 删除 临时的 id 属性
+            .attr("id",null);
+    });
+});
---
+++ assets/main/js/move.js
@@ -0,0 +1,66 @@
+
+
+$(document).ready(function () {
+    // bg scroll
+    var _bg_move = false; //移动标记
+    var _move_x, _move_y; //鼠标离控件左上角的相对位置
+    /*
+        如果是 label 的话 我们就直接跳过 上面的
+     */
+    var _isLabel=false;
+    $(".bg_show").mousedown(function (e) {
+        if(_isLabel) {
+            _bg_move = false;
+            return;
+        }else{
+            _bg_move = true;
+        }
+        _move_x = e.pageX - parseInt($(".bg_show").css("left"));
+     //   _move_y = e.pageY - parseInt($(".bg_show").css("top"));
+    });
+    $(document).mousemove(function (e) {
+        if (_bg_move) {
+            var x = e.pageX - _move_x; //移动时根据鼠标位置计算控件左上角的绝对位置
+            //var y = e.pageY - _move_y;
+            if(x>0 || x<=-window.innerWidth){
+                return;
+            }
+            $(".bg_show").css({ /*top: y, */ left: x }); //控件新位置
+        }
+    }).mouseup(function () {
+        _bg_move = false;
+    });
+
+
+
+// 下面是 图标的移动
+    var _label_x,_label_y;
+    var _label_move = false;
+
+    //这一行是 为了动态绑定  具体 查看 on 方法
+    $("body").on("mousedown",".label_tag",function(e){
+       // alert("a");
+//    })
+//    $(".label_tag").mousedown(function(e){
+        _isLabel =true;
+        _label_move = true;
+        _bg_move =false;
+        // $(this) 选择的是 点击的那个
+        $(this).addClass("mouseMove");
+        _label_x = e.pageX - parseInt($(this).css("left"));
+        _label_y = e.pageY - parseInt($(this).css("top"));
+    });
+    $(document).mousemove(function(e) {
+        if (_label_move) {
+            _bg_move = false;
+            var x = e.pageX - _label_x; //移动时根据鼠标位置计算控件左上角的绝对位置
+            var y = e.pageY - _label_y;
+            $(".mouseMove").css({ top: y, left: x }); //控件新位置
+        }
+    }).mouseup(function() {
+        _isLabel = false;
+        _label_move = false;
+        $(".mouseMove").removeClass("mouseMove"); //松开鼠标后停止移动并恢复成不透明
+    });
+
+});
